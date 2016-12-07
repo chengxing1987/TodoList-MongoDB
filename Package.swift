3ad5b456d54e56a5a16b00c1942b4bf390d21576
Jessica Pittman <jpittma@us.ibm.com>
2016-12-07 17:35:09 -0600

    Updated to swift 3.0.1

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "TodoList",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 2),
     ]
 )
